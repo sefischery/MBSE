c03fbfaa4df88da671bd805d3dbb87f9bc28ca22
MatJorgensen <s164151@student.dtu.dk>
2020-10-22 22:29:26 +0200

    updated matplotlib for grid_with_solar.py

--- grid_with_solar.py
+++ grid_with_solar.py
@@ -15,7 +15,7 @@ SOLAR_COUNT = 1
 GRID_START= 500
 GRID_CAP = 10000
 RANDOM_SEED = 42
-SIM_TIME = 1440
+SIM_TIME = 60 * 60		# Time in seconds
 
 def consumer(env, grid):
 	while True:
@@ -49,10 +49,44 @@ env.run(until=SIM_TIME)
 
 # Plot results
 from matplotlib import pyplot as plt
-plt.plot(grid_levels, label="Grid levels")
+import numpy as np
+
+# Draw plot
+fig, ax = plt.subplots(1, 1, figsize=(16, 9), dpi=80)
+ax.fill_between(np.arange(0, SIM_TIME), y1=grid_levels, y2=0, label="Grid levels", alpha=0.5, color='tab:red', linewidth=2)
+plt.hlines(MAX_THRESHOLD, 0, SIM_TIME, label="Maximum levels", colors='black', linestyles='--')
+plt.hlines(MIN_THRESHOLD, 0, SIM_TIME, label="Minimum levels", colors='black', linestyles='--')
+
+# Decorations
+ax.set_title('Simulation of Power Generated From Solar Panels (Random Values)', fontsize=16)
+ax.set(ylim=[0, 2000])
+ax.set_xlabel(r'Time [minutes]')
+ax.set_ylabel(r'Grid levels')
+ax.legend(loc='best', fontsize=12)
+plt.xticks(np.arange(0, SIM_TIME + 1, 60), fontsize=10, horizontalalignment='center')
+ax.set_xticklabels(np.arange(0, 60 + 1))
+plt.xlim(0, 3600)
+
+# Draw tick lines
+for y in np.arange(0, 2000, 200):
+    plt.hlines(y, xmin=0, xmax=len(np.arange(0, SIM_TIME)), colors='black', alpha=0.3, linestyles='--', lw=0.5)
+
+# Ligten borders
+plt.gca().spines["top"].set_alpha(0)
+plt.gca().spines["bottom"].set_alpha(.3)
+plt.gca().spines["right"].set_alpha(0)
+plt.gca().spines["left"].set_alpha(.3)
+
+# Show plot
+fig.tight_layout()
+plt.show()
+
+# Plot results
+#from matplotlib import pyplot as plt
+#plt.plot(grid_levels, label="Grid levels")
 plt.hlines(MAX_THRESHOLD, 0, SIM_TIME, label="Maximum levels")
 plt.hlines(MIN_THRESHOLD, 0, SIM_TIME, label="Minimum levels")
-plt.xlabel("Time")
-plt.ylabel("Grid levels")
-plt.legend()
-plt.show()
+#plt.xlabel("Time")
+#plt.ylabel("Grid levels")
+#plt.legend()
+#plt.show()
