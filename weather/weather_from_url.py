9221c9781906a3c3942fffa752e7f3c1102d74a8
Adam Bischoff <s174295@student.dtu.dk>
2020-10-25 12:19:36 +0100

    Added weather data of entire 2019 as csv

--- weather/weather_from_url.py
+++ weather/weather_from_url.py
@@ -1,37 +1,84 @@
 import requests
+import pandas as pd
+import datetime
 from requests.exceptions import HTTPError
 
-start_date = "2020-10-21"
-end_date = "2020-10-22"
+def gather_weather_json(start_datetime, end_datetime):
+        
+    date_format = "%Y-%m-%d"
+    start_date = start_datetime.strftime(date_format)
+    end_date = end_datetime.strftime(date_format)
 
-api_key = "3005becb7e874467b183c6d4158ad28b"
-latitude = "55.784"
-longitude = "12.519"
-frequency = "hourly"
-
-
-url = "https://api.weatherbit.io/v2.0/history/" + frequency \
-    + "?lat=" + latitude \
-    + "&lon=" + longitude \
-    + "&start_date=" + start_date \
-    + "&end_date=" + end_date \
-    + "&tz=local&key=" + api_key \
+    #api_key = "3005becb7e874467b183c6d4158ad28b"
+    api_key = "d3493c8dbeb448fbbe804c07a1b11a7b"
+    latitude = "55.784"
+    longitude = "12.519"
+    frequency = "hourly"
 
 
-
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-
-    jsonResponse = response.json()
-except HTTPError as http_err:
-    print(f"HTTP error occurred: {http_err}")
-except Exception as err:
-    print(f"Other error occured: {err}")
+    url = "https://api.weatherbit.io/v2.0/history/" + frequency \
+        + "?lat=" + latitude \
+        + "&lon=" + longitude \
+        + "&start_date=" + start_date \
+        + "&end_date=" + end_date \
+        + "&tz=local&key=" + api_key \
 
 
-for item in jsonResponse["data"]:
-    print("Time: " + str(item["timestamp_local"]))
-    print("wind: " + str(item["wind_spd"]))
-    print("solar radiation: " + str(item["solar_rad"]))
-    print("\n")
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        jsonResponse = response.json()
+    except HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occured: {err}")
+
+    return jsonResponse
+
+    # for item in jsonResponse["data"]:
+    #     print("Time: " + str(item["timestamp_local"]))
+    #     print("wind: " + str(item["wind_spd"]))
+    #     print("solar radiation: " + str(item["solar_rad"]))
+    #     print("\n")
+
+start_datetime = datetime.datetime(2019, 1, 1)
+#dataframe = pd.read_csv("weather.csv", sep=';')
+dataframe = pd.DataFrame()
+
+for i in range(365):
+    weather = gather_weather_json(start_datetime, start_datetime + datetime.timedelta(days=1))
+
+    new_data = pd.DataFrame(weather["data"])
+    dataframe = dataframe.append(new_data, ignore_index = True)
+
+    start_datetime += datetime.timedelta(days=1)
+    print(i)
+
+dataframe.to_csv('weather.csv', sep=';')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(weather)
+# print()
+
+# for item in weather["data"]:
+#     print("Time: " + str(item["timestamp_local"]))
+#     print("wind: " + str(item["wind_spd"]))
+#     print("solar radiation: " + str(item["solar_rad"]))
+#     print("\n")
