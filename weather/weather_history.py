ad7f5063e549d6da301d552f7e682e95c7680260
Adam Bischoff <s174295@student.dtu.dk>
2020-10-27 14:47:28 +0100

    Smoothed out weather log data

--- weather/weather_history.py
+++ weather/weather_history.py
@@ -6,10 +6,24 @@ class WeatherLog:
         self.dataFrame = pd.read_csv(csv_path, sep=';', index_col="timestamp_local")
 
     def get_solar_rad(self, time):
-        return self.dataFrame.loc[self.__datetime_to_key_string(time), 'solar_rad']
+        solar_before = self.dataFrame.loc[self.__datetime_to_key_string(time), 'solar_rad']
+        solar_after  = self.dataFrame.loc[self.__datetime_to_key_string(time + datetime.timedelta(hours=1)), 'solar_rad']
+        solar_change = solar_after - solar_before
+
+        hour_completion = time.minute / 60.0
+
+        return solar_before + (solar_change * hour_completion)
+
+
 
     def get_wind_speed(self, time):
-        return self.dataFrame.loc[self.__datetime_to_key_string(time), 'wind_spd']
+        wind_before = self.dataFrame.loc[self.__datetime_to_key_string(time), 'wind_spd']
+        wind_after  = self.dataFrame.loc[self.__datetime_to_key_string(time + datetime.timedelta(hours=1)), 'wind_spd']
+        wind_change = wind_after - wind_before
+
+        hour_completion = time.minute / 60.0
+
+        return wind_before + (wind_change * hour_completion)
 
     def __datetime_to_key_string(self, dt):
         return dt.strftime("%Y-%m-%dT%H:00:00")
@@ -17,7 +31,7 @@ class WeatherLog:
 
 # log = WeatherLog("weather_copenhagen.csv")
 
-# date = datetime.datetime(2019, 1, 1, 11)
+# date = datetime.datetime(2019, 1, 1, 11, 30)
 # print(log.get_solar_rad(date))
 # print(log.get_wind_speed(date))
 # print(type(log.get_solar_rad(date)))
