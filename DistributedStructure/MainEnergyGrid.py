a88d6ccbd14dbed92f5379ffb6db8e1695aa81cc
glasdam95 <glasdam95@gmail.com>
2020-11-01 23:47:42 +0100

    Update city energy distribution

--- DistributedStructure/MainEnergyGrid.py
+++ DistributedStructure/MainEnergyGrid.py
@@ -7,8 +7,8 @@ from DistributedStructure.Consumer import Consumer, SelectRandomConsumerType, Se
 from DistributedStructure.WindTurbine import WindTurbine
 
 SIM_TIME = 24
-NUMB_OF_CITIES = 4
-NUMB_OF_WINDTURBINES = 5
+NUMB_OF_CITIES = 10
+NUMB_OF_WINDTURBINES = 10
 
 WING_SIZE = [20, 80]
 
@@ -40,8 +40,32 @@ class EnergyGrid(object):
             supportiveCities = []
             for city in self.cities:
                 if city.battery.level / city.battery.capacity < 0.1:
-                    print(f"City number: {city.cityNumber} enter critical level")
+                    criticalCities.append(city) # Critical cities
+                elif city.battery.level / city.battery.capacity > 0.20:
+                    supportiveCities.append(city) # Balanced city energy level
+            if len(criticalCities) > 0 and len(supportiveCities) > 0:
+                env.process(self.PerformCityEnergyDistribution(criticalCities, supportiveCities))
             yield self.env.timeout(1)
+
+    def PerformCityEnergyDistribution(self, criticalCities, supportiveCities):
+        for criticalCity in criticalCities:
+            neededEnergy = ((criticalCity.battery.capacity * 0.15)-criticalCity.battery.level)
+            for supportiveCity in supportiveCities:
+                if neededEnergy > 0:
+                    surPlusEnergy = (supportiveCity.battery.level-(supportiveCity.battery.capacity * 0.20))
+                    if surPlusEnergy > 0:
+                        if surPlusEnergy < neededEnergy:
+                            neededEnergy -= surPlusEnergy
+                            supportiveCity.battery.get(surPlusEnergy)
+                            criticalCity.battery.put(surPlusEnergy)
+                            print(f"City {supportiveCity.cityNumber}, sends energy level {surPlusEnergy}, to city {criticalCity.cityNumber}")
+                        else:
+                            supportiveCity.battery.get(neededEnergy)
+                            criticalCity.battery.put(neededEnergy)
+                            print(f"City {supportiveCity.cityNumber}, sends energy level {neededEnergy}, to city {criticalCity.cityNumber}")
+                            neededEnergy -= neededEnergy
+
+        yield self.env.timeout(1)
 
     def GetGeneratedEnergy(self):
         while True:
@@ -82,11 +106,11 @@ for city in VirtualPowerGrid.cities:
     for consumer in consumers:
         resource = SelectRandomResourceType()
         consumer.SetResource(resource)  # Set generation resource
-        consumer.setResourceSize(random.randint(10, 70))  # set size of resource
+        consumer.setResourceSize(random.randint(10, 80))  # set size of resource
         city.AddConsumer(consumer)
 
     batteryCapacity = len(city.consumerList) * 5000
-    cityBatteryContainer = simpy.Container(env, batteryCapacity, init=batteryCapacity/3)
+    cityBatteryContainer = simpy.Container(env, batteryCapacity, init=0.35 * batteryCapacity)
     city.AddBattery(cityBatteryContainer)
 
 # Execute!
@@ -94,8 +118,9 @@ env.run(until=SIM_TIME)
 
 # Printer functionality
 for city in VirtualPowerGrid.cities:
+    print()
     print(
-        f"City: {city.cityNumber}, number of consumers in city: {len(city.consumerList)}, city battery level: {city.battery.level}, battery start level: {city.battery.capacity}")
+        f"City: {city.cityNumber}, number of consumers in city: {len(city.consumerList)}, city battery level: {city.battery.level}, battery max capacity: {city.battery.capacity}")
     cityConsumption = 0
     for consumer in city.consumerList:
         cityConsumption += consumer.consumedEnergy
