8e6ed070105edf50f65d3654dc8e9993228d8729
MatJorgensen <s164151@student.dtu.dk>
2020-11-01 23:30:00 +0100

    drafted graph structure for energy distribution

---
+++ DistributedStructure/Infrastructure.py
@@ -0,0 +1,64 @@
+import pprint
+from collections import defaultdict
+
+
+class Graph(object):
+    """ Graph data structure, undirected by default. """
+
+    def __init__(self, connections):
+        self._graph = defaultdict(set)
+        self.add_connections(connections)
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self._graph))
+
+    def add_connections(self, connections):
+        """ Add connections (list of tuple pairs) to graph """
+
+        for node1, node2 in connections:
+            self.add(node1, node2)
+
+    def add(self, node1, node2):
+        """ Add connection between node1 and node2 """
+
+        self._graph[node1].add(node2)
+        self._graph[node2].add(node1)
+
+    def remove(self, node):
+        """ Remove all references to node """
+
+        for n, cxns in self._graph.items():  # python3: items(); python2: iteritems()
+            try:
+                cxns.remove(node)
+            except KeyError:
+                pass
+        try:
+            del self._graph[node]
+        except KeyError:
+            pass
+
+    def is_connected(self, node1, node2):
+        """ Is node1 directly connected to node2 """
+
+        return node1 in self._graph and node2 in self._graph[node1]
+
+    def find_path(self, node1, node2, path=[]):
+        """ Find any path between node1 and node2 (may not be shortest) """
+
+        path = path + [node1]
+        if node1 == node2:
+            return path
+        if node1 not in self._graph:
+            return None
+        for node in self._graph[node1]:
+            if node not in path:
+                new_path = self.find_path(node, node2, path)
+                if new_path:
+                    return new_path
+        return None
+
+
+connections = [('A', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('E', 'F'), ('F', 'C')]
+g = Graph(connections)
+pretty_print = pprint.PrettyPrinter()
+pretty_print.pprint(g._graph)
