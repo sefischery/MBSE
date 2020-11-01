bbfc20c0147d2d41b1b07a79614590b70f71625f
SebastianFischer164158 <s164158@student.dtu.dk>
2020-11-01 11:07:32 +0100

    weather_history: changed _date_time to a static method, and pep errors

--- weather/weather_history.py
+++ weather/weather_history.py
@@ -1,33 +1,37 @@
 import pandas as pd
 import datetime
 
+
 class WeatherLog:
     def __init__(self, csv_path):
-        self.dataFrame = pd.read_csv(csv_path, sep=';', index_col="timestamp_local")
+        self.dataFrame = pd.read_csv(csv_path, sep=';',
+                                     index_col="timestamp_local")
 
     def get_solar_rad(self, time):
-        solar_before = self.dataFrame.loc[self.__datetime_to_key_string(time), 'solar_rad']
-        solar_after  = self.dataFrame.loc[self.__datetime_to_key_string(time + datetime.timedelta(hours=1)), 'solar_rad']
+        solar_before = self.dataFrame.loc[
+            self._datetime_to_key_string(time), 'solar_rad']
+        solar_after = self.dataFrame.loc[self._datetime_to_key_string(
+            time + datetime.timedelta(hours=1)), 'solar_rad']
         solar_change = solar_after - solar_before
 
         hour_completion = time.minute / 60.0
 
         return solar_before + (solar_change * hour_completion)
 
-
-
     def get_wind_speed(self, time):
-        wind_before = self.dataFrame.loc[self.__datetime_to_key_string(time), 'wind_spd']
-        wind_after  = self.dataFrame.loc[self.__datetime_to_key_string(time + datetime.timedelta(hours=1)), 'wind_spd']
+        wind_before = self.dataFrame.loc[
+            self._datetime_to_key_string(time), 'wind_spd']
+        wind_after = self.dataFrame.loc[self._datetime_to_key_string(
+            time + datetime.timedelta(hours=1)), 'wind_spd']
         wind_change = wind_after - wind_before
 
         hour_completion = time.minute / 60.0
 
         return wind_before + (wind_change * hour_completion)
 
-    def __datetime_to_key_string(self, dt):
+    @staticmethod
+    def _datetime_to_key_string(dt):
         return dt.strftime("%Y-%m-%dT%H:00:00")
-
 
 # log = WeatherLog("weather_copenhagen.csv")
 
