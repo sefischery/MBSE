ce909de3bcadfe2422785340669130b5bda3e936
SebastianFischer164158 <s164158@student.dtu.dk>
2020-11-01 11:04:47 +0100

    WindTurbine: introduced relative path of weather file
    and some pep8 errors

--- DistributedStructure/WindTurbine.py
+++ DistributedStructure/WindTurbine.py
@@ -1,11 +1,15 @@
+from pathlib import Path
 from weather import weather_history
 import numpy as np
+
+base_path = Path(__file__).parent
+file_path = (base_path / "../weather_copenhagen.csv").resolve()
 
 
 class WindTurbine(object):
     def __init__(self, wing_size):
         self.A = pow(wing_size, 2) * np.pi
-        self.log = weather_history.WeatherLog("C:\\Users\\s164156\\Documents\\MBSE-master\\weather_copenhagen.csv")
+        self.log = weather_history.WeatherLog(file_path)
 
     # https://www.ajdesigner.com/phpwindpower/wind_generator_power_performance_coefficient.php
     # Info about the below variables and their typical values
@@ -13,12 +17,12 @@ class WindTurbine(object):
     # Air density in kg/m3
     p = 1.23
     # Rotor swept area in m3:
-    #A = wing_size * 2 * pow(numpy.pi, 2)
+    # A = wing_size * 2 * pow(numpy.pi, 2)
     # Coefficient of performance:
     # Typical value is 0.35. The theoretical max is 0.56.
     Cp = 0.35
     # Wind speed in m/s:
-    #V = self.log.get_wind_speed(datetime)
+    # V = self.log.get_wind_speed(datetime)
 
     # Generator efficiency:
     # Typically between 50 and 80 %
@@ -28,5 +32,6 @@ class WindTurbine(object):
     Nb = 0.95
 
     def power(self, datetime):
-        power = 0.5 * self.p * self.A * self.Cp * pow(self.log.get_wind_speed(datetime), 3) * self.Ng * self.Nb
+        power = 0.5 * self.p * self.A * self.Cp * pow(
+            self.log.get_wind_speed(datetime), 3) * self.Ng * self.Nb
         return power / 10000
