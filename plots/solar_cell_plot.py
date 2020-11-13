accc5d7ca04b99ef07b5c77c4acf5c6087752176
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-11-13 19:19:17 +0100

    A single solar cell plot

---
+++ plots/solar_cell_plot.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt, patches as pa
+import seaborn as sns
+import numpy as np
+import datetime
+
+from pathlib import Path
+from weather import weather_history
+
+from DistributedStructure.SolarCell import SolarCell
+
+base_path = Path(__file__).parent
+file_path = (base_path / "../weather/data/2019/weather_copenhagen.csv").resolve()
+
+log = weather_history.WeatherLog(file_path)
+sns.set_theme()
+
+solar_cell = SolarCell(25)
+days = list(map(lambda x: solar_cell.power(datetime.datetime(2019,1,1,0) + datetime.timedelta(hours=int(x))), np.arange(364*24)))
+
+plt.figure(figsize=(20, 10))
+
+sns.set(font_scale=5)
+plt.fill_between(np.arange(364*24), days, color='g')
+plt.xlabel("Time of year [month]")
+plt.ylabel("Power generated [Watt]")
+plt.xticks(np.arange(15*24, 364*24, 30*24), (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+plt.show()
