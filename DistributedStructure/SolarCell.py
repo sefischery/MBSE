2be69d5227dc8181688c5e745db4a5031a1e73bd
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-11-13 12:05:02 +0100

    Record solar cell generation

--- DistributedStructure/SolarCell.py
+++ DistributedStructure/SolarCell.py
@@ -13,8 +13,16 @@ class SolarCell(object):
         self.log = weather_history.WeatherLog(file_path)
         self.squaremeters = squaremeters_size
 
+        # Record total power produced by solar cell
+        self.totalProduces = 0
+
+        # Keep the efficiency of the individuel solar cell constant
+        self.efficiency = random.uniform(0.175, 0.225)
+
     def __repr__(self):
         return '{}({})'.format(self.__class__.__name__, self.id)
 
     def power(self, datetime):
-        return self.log.get_solar_rad(datetime) * random.uniform(0.175, 0.225) * self.squaremeters
+        power_generated =  self.log.get_solar_rad(datetime) * self.efficiency * self.squaremeters
+        self.totalProduces += power_generated
+        return power_generated
