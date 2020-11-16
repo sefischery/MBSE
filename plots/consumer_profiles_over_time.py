acb54fe6ff3a5d988ccb80bbaea960625f837570
MatJorgensen <s164151@student.dtu.dk>
2020-11-16 16:41:25 +0100

    updated plot consumer_profiles_over_time.py to be cumulative

--- plots/consumer_profiles_over_time.py
+++ plots/consumer_profiles_over_time.py
@@ -15,22 +15,22 @@ def convert_camel_case(string):
 
     return switcher.get(string, 'error')
 
+
 # Meta data
 with open("../plots/output.json", "r") as f:
     VirtualPowerGrid = json.load(f)
 
 # Colors (to match DTU color palette)
-colors = ['#030F4F',  # Navy blue
-          '#2F3EEA',  # Blue
-          '#79238E',  # Purple
-          '#E83F48',  # Red
+colors = ['#2F3EEA',  # Blue
           '#FC7634',  # Orange
+          '#008835',  # Green
+          '#E83F48',  # Red
+          '#79238E',  # Purple
           '#F7BBB1',  # Pink
           '#F6D04D',  # Yellow
           '#1FD082',  # Bright Green
-          '#008835',  # Green
+          '#030F4F',  # Navy blue
           '#DADADA']  # Grey
-
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=colors)
 
 # Data
@@ -43,22 +43,30 @@ for city in VirtualPowerGrid['cities']:
         else:
             consumer_profiles[consumer['type']] = [b for a, b in consumer['consumedEnergyHistory']]
 
-# Plot metrics
-opacity = 1
+# Create cumulative data
+data_cumsum = np.cumsum([values for values in consumer_profiles.values()], axis=0)
+
+# Assign
+for i, (profile, _) in enumerate(consumer_profiles.items()):
+    consumer_profiles[profile] = data_cumsum[i][:]
 
 # Plot
 fig, ax = plt.subplots(1, 1, figsize=(16, 9), dpi=80)
-for profile, data in consumer_profiles.items():
-    plt.plot(data[:24 + 1], alpha=opacity, label=convert_camel_case(profile))
-plt.xlabel('Time [hours]')
+
+# Plot in reverse order from largest cumulative values to lowest
+for profile, data in reversed(consumer_profiles.items()):
+    plt.plot(data[:24 + 1], alpha=1, label=convert_camel_case(profile), linewidth=2)
+    ax.fill_between(np.arange(24 + 1), y1=data[:24 + 1], y2=0, alpha=0.8)
+plt.xlabel('Time of day')
 plt.ylabel('Power [kW]')
-plt.title('Power usage of user profiles over time', fontsize=16)
+plt.title('Daily power usage pattern for the four user profiles', fontsize=16)
 plt.xticks(np.arange(0, 24 + 1), [f'{str(i).zfill(2)}:00' for i in np.pad(np.arange(24), (0, 1))], fontsize=10)
-plt.yticks(plt.yticks()[0], [int(i/1000) for i in plt.yticks()[0]], fontsize=10)
+plt.yticks(plt.yticks()[0], [int(i / 1000) for i in plt.yticks()[0]], fontsize=10)
 plt.xlim([0, 24])
+plt.ylim([0, plt.yticks()[0][-1]])
 plt.grid(True, which='both', color="#93a1a1", alpha=0.3)
 plt.legend(loc='best', fontsize=12)
 
 plt.tight_layout()
 plt.show()
-#plt.savefig('consumer_profiles_over_time.pdf', format='pdf')   # (uncomment to save plot)
+# plt.savefig('consumer_profiles_over_time.pdf', format='pdf')   # (uncomment to save plot)
