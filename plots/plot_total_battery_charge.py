99af3aea79d95bc9f44eab96434a23cc904ed9e7
Mathias Boss Jørgensen <s164151@student.dtu.dk>
2020-12-04 15:02:01 +0100

    updated plot_total_battery_charge

--- plots/plot_total_battery_charge.py
+++ plots/plot_total_battery_charge.py
@@ -5,12 +5,12 @@ import json
 import brewer2mpl
 
 # Load data
-with open("plots/output.json", "r") as f:
+with open('../plots/output.json', 'r') as f:
     VirtualPowerGrid = json.load(f)
 # Generate plot data
-sim_time = VirtualPowerGrid["sim_time"]
-cities = VirtualPowerGrid["cities"]
-resource_battery_history = VirtualPowerGrid["resource_battery_history"]
+sim_time = VirtualPowerGrid['sim_time']
+cities = VirtualPowerGrid['cities']
+resource_battery_history = VirtualPowerGrid['resource_battery_history']
 city1History = []
 city4History = []
 battery_charge_sum = []
@@ -18,18 +18,18 @@ battery_charge_consumers = []
 for hour in range(sim_time):
     charge = 0
     for city in cities:
-        charge += city["city_battery_level"][hour]
-        if city["city_number"] == 1:
-            city1History.append(city["city_battery_level"][hour])
-        if city["city_number"] == 4:
-            city4History.append(city["city_battery_level"][hour])
+        charge += city['city_battery_level'][hour]
+        if city['city_number'] == 1:
+            city1History.append(city['city_battery_level'][hour])
+        if city['city_number'] == 4:
+            city4History.append(city['city_battery_level'][hour])
     battery_charge_consumers.append(charge)
     charge += resource_battery_history[hour]
     battery_charge_sum.append(charge)
 
-print("Minimum charge measured: " + str(min(battery_charge_sum)))
-print("Maximum charge measured: " + str(max(battery_charge_sum)))
-print("Difference: " + str(max(battery_charge_sum) - min(battery_charge_sum)))
+print('Minimum charge measured: ' + str(min(battery_charge_sum)))
+print('Maximum charge measured: ' + str(max(battery_charge_sum)))
+print('Difference: ' + str(max(battery_charge_sum) - min(battery_charge_sum)))
 
 outages = np.zeros(sim_time)
 overloads = np.zeros(sim_time)
@@ -47,7 +47,7 @@ params = {
     'legend.fontsize': 8,
     'xtick.labelsize': 10,
     'ytick.labelsize': 10,
-    'figure.figsize': [3.5, 2.5]
+    'figure.figsize': [3.5, 3.5]
 }
 mpl.rcParams.update(params)
 # Plot
@@ -69,7 +69,7 @@ plt.plot(np.arange(0, sim_time), city4History, label='City 4')
 # Labels
 ax.set_xlabel('Time of year [hour]')
 ax.set_ylabel('Power stored [MWh]')
-#plt.xticks(np.arange(15 * 24, 364 * 24, 30 * 24), (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+# plt.xticks(np.arange(15 * 24, 364 * 24, 30 * 24), (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
 # Plot layout
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -84,10 +84,12 @@ for spine in ax.spines.values():
 # Put the grid behind
 ax.grid(axis='y', color="0.9", linestyle='-', linewidth=1)
 ax.set_axisbelow(True)
+# Edit legend
+legend = ax.legend(loc='best');
+frame = legend.get_frame()
+frame.set_facecolor('1.0')
+frame.set_edgecolor('1.0')
 fig.tight_layout()
 
-plt.legend(loc = 2, prop = {'size': 6})
-
 # plt.show()
-fig.savefig('output/Approximate-Battery.pdf')
-
+fig.savefig('output/approximate-battery.pdf')
